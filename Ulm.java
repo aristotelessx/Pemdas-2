cb61495a32bb1fd8965ace150ddeb5f7492d135a
aristotelessx <mnaufal2402@gmail.com>
2024-09-21 20:03:23 +0800

    Add files via upload

---
+++ Ulm.java
@@ -0,0 +1,16 @@
+package tipedata;
+
+public class Ulm {
+
+    public static void main(String[] args) {
+        String ulm = "Halo ULM";
+        String visiUlm = "ULM Berdaya Saing dan Unggul di Bidang Lahan Basah";
+        String visiIlkom = "Prodi Ilkom ULM Unggul dengan Teknologi Kekinian";
+
+        System.out.println(ulm);
+        System.out.println(visiUlm);
+        System.out.println(visiIlkom);
+        System.out.println("------");
+        System.out.println(ulm + "\n\t" + visiUlm + "\n\t\t" + visiIlkom);
+    }
+}
